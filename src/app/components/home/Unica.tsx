84c21dc4d9379a956b4edb37f5a1cf477053ff2e
vttdrddevelopment <vittoriaedgardo1994@gmail.com>
2025-03-10 14:29:08 +0100

    fix responsive sezione unica

--- src/app/components/home/Unica.tsx
+++ src/app/components/home/Unica.tsx
@@ -1,15 +1,15 @@
 export default function Unica() {
   return (
-    <div className="flex h-[60vh] flex-col lg:flex-row justify-center gap-10 padding py-10 bg-fixed bg-primary unica">
-      <div className="flex flex-col w-1/2 justify-center">
+    <div className="flex lg:h-[60vh] h-[80vh] flex-col lg:flex-row justify-center gap-10 padding py-10 bg-fixed bg-primary unica">
+      <div className="flex flex-col lg:w-1/2 justify-center lg:items-start items-center">
         <h1 className="text-[120px] uppercase text-white font-semibold">
           Unica
         </h1>
-        <span className="text-4xl text-white font-bold uppercase leading-10">
+        <span className="text-4xl text-center lg:text-start text-white font-bold uppercase leading-10">
           La nuova finestra <br /> di ponzio
         </span>
       </div>
-      <div className="relative lg:w-1/2 flex justify-center items-center">
+      <div className="relative lg:w-1/2 h-full w-full flex justify-center items-center">
       <iframe
         src="https://player.vimeo.com/video/1064270242"
         style={{
