fa76253ad5614f213e3db0dc63b39ee9a39e4240
vttdrddevelopment <vittoriaedgardo1994@gmail.com>
2025-03-21 11:31:17 +0100

    fix immagini altre sezioni

--- src/app/contatti/page.tsx
+++ src/app/contatti/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import homebg from "../../../public/home.jpg";
+import homebg from "../../../public/bgprodotti.png";
 import contatti1 from "../../../public/contatti1.jpg";
 import contatti2 from "../../../public/contatti2.jpg";
 import contatti3 from "../../../public/contatti3.jpg";
@@ -10,10 +10,10 @@ import FAQ from "../components/home/FAQ";
 export default function page() {
   return (
     <div className="font-[family-name:var(--font-plus-jakarta-sans)]">
-      <div className="grid grid-rows-[20px_1fr_20px] relative h-[50vh] p-8 pb-20 gap-16 padding">
-      <Image src={homebg} alt="alt" layout="fill" objectFit="cover" objectPosition="bottom"  className="w-full absolute top-0 h-[50vh]"/>
-        <div className="absolute top-0 w-full h-[50vh] bg-gradient-to-b from-red-900 to-gray-800 opacity-50 z-10"></div>
-        <div className="flex flex-col gap-4 justify-center items-center w-full h-[50vh]">
+      <div className="grid grid-rows-[20px_1fr_20px] relative h-[80vh] p-8 pb-20 gap-16 padding">
+      <Image src={homebg} alt="alt" layout="fill" objectFit="cover" objectPosition="center"  className="w-full absolute top-0 h-[80vh]"/>
+      <div className="absolute top-0 w-full h-[80vh] bg-gradient-to-t from-transparent to-red-950 opacity-50 z-10"></div>
+        <div className="flex flex-col gap-4 justify-center items-center w-full h-[80vh]">
           <h1 className="text-white font-bold xl:text-[85px] lg:text-[70px] md:text-[50px] text-[40px] z-20 xl:leading-20 lg:leading-16 leading-10">
             Contatti
           </h1>
--- src/app/prodotti/page.tsx
+++ src/app/prodotti/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
 
-import homebg from "../../../public/home.jpg";
+import homebg from "../../../public/bgprodotti.png";
 
 import { IoChevronBackCircle, IoChevronForwardCircle } from "react-icons/io5";
 import Link from "next/link";
@@ -37,10 +37,10 @@ export default function page() {
 
   return (
     <div className="font-[family-name:var(--font-plus-jakarta-sans)]">
-      <div className="grid grid-rows-[20px_1fr_20px] relative h-[50vh] p-8 pb-20 gap-16 padding">
-      <Image src={homebg} alt="alt" layout="fill" objectFit="cover" objectPosition="bottom"  className="w-full absolute top-0 h-[50vh]"/>
-        <div className="absolute top-0 w-full h-[50vh] bg-gradient-to-b from-red-900 to-gray-800 opacity-50 z-10"></div>
-        <div className="flex flex-col gap-4 justify-center items-center w-full h-[50vh]">
+      <div className="grid grid-rows-[20px_1fr_20px] relative h-[80vh] p-8 pb-20 gap-16 padding">
+      <Image src={homebg} alt="alt" layout="fill" objectFit="cover" objectPosition="center"  className="w-full absolute top-0 h-[80vh]"/>
+        <div className="absolute top-0 w-full h-[80vh] bg-gradient-to-t from-transparent to-red-950 opacity-50 z-10"></div>
+        <div className="flex flex-col gap-4 justify-center items-center w-full h-[80vh]">
           <h1 className="text-white font-bold xl:text-[85px] lg:text-[70px] md:text-[50px] text-[40px] z-20 xl:leading-20 lg:leading-16 leading-10">
             Prodotti
           </h1>
